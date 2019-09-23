1b3c97a8575a042125925a66c13dd53f90adb69d
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-09-23 17:39:14 +0200

    risoluzione conflitti

--- es_4/main.js
+++ es_4/main.js
@@ -1,13 +1,12 @@
 var utenti = ["root", "Andrea", "Sofia", "Silvia", "Pasquale"];
 var lun = utenti.length;
 
-document.getElementById("nome_utente").innerHTML = "Inserire il nome utente";
-
-function controllo(nome_utente){
+function controllo(){
+	var entry = document.getElementById("utente").value;
 	for(i = 0; i < lun; i++){
 		var nome = utenti[i];
-		if(nome_utente==nome){
-			window.location = "https://www.google.it";
+		if(entry==nome){
+			
 		}
 	}
 }

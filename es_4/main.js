37cc96813b17030289ae0b225d9110946e8e915e
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-09-23 16:52:12 +0200

    aggiungo file .js

--- es_4/main.js
+++ es_4/main.js
@@ -1,1 +1,13 @@
-ciao
+var utenti = ["root", "Andrea", "Sofia", "Silvia", "Pasquale"];
+var lun = utenti.length;
+
+document.getElementById("nome_utente").innerHTML = "Inserire il nome utente";
+
+function controllo(nome_utente){
+	for(i = 0; i < lun; i++){
+		var nome = utenti[i];
+		if(nome_utente==nome){
+			window.location = "https://www.google.it";
+		}
+	}
+}

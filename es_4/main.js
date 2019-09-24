0d46ec4664ac0b641aabafa017e4787082f8cf56
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-09-24 09:28:46 +0200

    Aggiunta funzione login

--- es_4/main.js
+++ es_4/main.js
@@ -1,6 +1,7 @@
 var utenti = ["root", "Andrea", "Sofia", "Silvia", "Pasquale"];
 var psw = ["root", "Ciao123", "qwerty", "12345", "asdfg"];
 var lun = utenti.length;
+var lung = psw.length;
 
 function controllo(){
 	var entry = document.getElementById("utente").value;
@@ -8,6 +9,18 @@ function controllo(){
 		var nome = utenti[i];
 		if(entry==nome){
 			console.log("Accesso Effettuato");
+		}
+	}
+}
+
+function login(){
+	var entry = document.getElementById("utente").value;
+	var contr = document.getElementById("pass").value;
+	for(i = 0; i < lun; i++){
+		var nome = utenti[i];
+		var pass = psw[i];
+		if(entry == nome && contr == pass){
+			console.log("Login Effettuato");
 		}
 	}
 }
